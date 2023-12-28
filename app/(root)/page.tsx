9d6180a680ca8397890875d236731092d45ea1b5
Bruno <warren@Warrens-MacBook-Pro.local>
2023-12-28 23:25:07 +0000

    created layout of page different areas

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,12 @@
-import { UserButton } from "@clerk/nextjs";
+// import { UserButton } from "@clerk/nextjs";
 
 const Home = () => {
   return (
-    <main>
-      <h1>Home</h1>
-      <UserButton afterSignOutUrl="/"/>
-    </main>
-  )
-}
+    <>
+      <h1 className="head-text text-left">Home</h1>
+      {/* <UserButton afterSignOutUrl="/"/> */}
+    </>
+  );
+};
 
-export default Home
+export default Home;
