cf00e1fa6665efc0d308c1c77bce4281f7c41bc8
Bruno <warren@Warrens-MacBook-Pro.local>
2023-12-29 15:44:16 +0000

    added additional feature to leftbar

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,34 +1,60 @@
+"use client";
+
 import { sidebarLinks } from "@/constants";
+import { SignOutButton, SignedIn } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useRouter } from "next/router";
 
 export const LeftSidebar = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+
   return (
     <section className="custom-scrollbar leftsidebar">
       <div className="flex w-full flex-1 flex-col gap-6 px-6">
         {sidebarLinks.map((link: any) => {
+          console.log(pathname);
 
-          const isActive = () => {
-            
-          }
+          const linkIsActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route; // way to check if on the page of link clicked on
 
           return (
             <Link
               href={link.route}
               key={link.label}
-              className="leftsidebar_link"
+              className={`leftsidebar_link ${linkIsActive && `bg-primary-500`}`}
             >
               <Image
                 src={link.imgURL}
                 alt={link.label}
                 width={24}
-                height={24}
+                height={ 24}
               />
 
               <p className="text-light-1 max-lg:hidden">{link.label}</p>
             </Link>
           );
         })}
+      </div>
+
+      <div className="mt-10 px-6">
+        <SignedIn>
+          <SignOutButton signOutCallback={() => router.push('/signin')}>
+            <div className="flex cursor-pointer gap-4 p-4">
+              <Image
+                src="/assets/logout.svg"
+                alt="logout"
+                width={24}
+                height={24}
+              />
+
+              <p className="text-light-2 max-lg:hideen">Logout</p>
+            </div>
+          </SignOutButton>
+        </SignedIn>
       </div>
     </section>
   );
